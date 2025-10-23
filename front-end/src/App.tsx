9e052339902918e07f33f1c2585ee3329dd9ee19
tuvananh <tuvananh.dmx@gmail.com>
2025-10-23 17:39:07 +0700

    toi vua lam so luoc login duoc va mot chut giao dien

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,30 @@
-import Nav from "./components/Nav"
-function App() {
+// src/App.tsx
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/Login";
+import HomePage from "./pages/Home";
 
+const App: React.FC = () => {
   return (
-    <Nav />
-  )
-}
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={
+              <HomePage />
+            } />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
