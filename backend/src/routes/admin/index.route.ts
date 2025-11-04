dfbe12f6cf99cb80f6cc412c41e34ef6a2da25fe
Hoang Minh Vu <minhvu82004.com>
2025-11-04 17:28:29 +0700

    hotfix:adminroute

--- backend/src/routes/admin/index.route.ts
+++ backend/src/routes/admin/index.route.ts
@@ -1,9 +1,6 @@
 import express from "express";
-import { verifyToken } from "../../middlewares/auth.middleware";
-import { roleCheck } from "../../middlewares/roleCheck.middleware";
-
-
 import userRoutes from "./user.route";
+// import propertyRoutes from "./property.route";
 import cityRoutes from "./city.route";
 import typeRoutes from "./type.route";
 import categoryRoutes from "./category.route";
@@ -12,11 +9,12 @@ import featureRoutes from "./feature.route";
 const router = express.Router();
 
 
-router.use(verifyToken, roleCheck("admin"));
+router.use("/", userRoutes);
+// router.use("/properties", propertyRoutes);
 
-router.use("/users", userRoutes);
 router.use("/cities", cityRoutes);
 router.use("/types", typeRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/features", featureRoutes);
+
 export default router;
