a807c8759c89b88f26e5ecd7ef049dd7aa21e5a9
Hoang Minh Vu <minhvu82004.com>
2025-11-04 17:04:40 +0700

    hotfix: admin route

--- backend/src/routes/admin/index.route.ts
+++ backend/src/routes/admin/index.route.ts
@@ -1,17 +1,22 @@
 import express from "express";
+import { verifyToken } from "../../middlewares/auth.middleware";
+import { roleCheck } from "../../middlewares/roleCheck.middleware";
 
-// Import các route con của admin
+
+import userRoutes from "./user.route";
 import cityRoutes from "./city.route";
 import typeRoutes from "./type.route";
-import featureRoutes from "./feature.route";
 import categoryRoutes from "./category.route";
+import featureRoutes from "./feature.route";
 
 const router = express.Router();
 
-// Kết nối các nhóm route
+
+router.use(verifyToken, roleCheck("admin"));
+
+router.use("/users", userRoutes);
 router.use("/cities", cityRoutes);
 router.use("/types", typeRoutes);
-router.use("/features", featureRoutes);
 router.use("/categories", categoryRoutes);
-
+router.use("/features", featureRoutes);
 export default router;
