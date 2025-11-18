66b1d662db2a45a953fa59ea14b8343ea5917344
viethuy0201 <huybabytong.vn@gmail.com>
2025-11-18 21:47:37 +0700

    update route

--- backend/src/routes/client/buyer/index.route.ts
+++ backend/src/routes/client/buyer/index.route.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import appointmentRoutes from "./appointment.route";
 import favoriteRoutes from "./favorite.route";
+import offersRoutes from "./offer.route"
+
 const router = express.Router();
 
 router.use("/appointments", appointmentRoutes);
 router.use("/favorites", favoriteRoutes);
+router.use("/offers", offersRoutes);
+
 export default router;
