a348308a6b2e303e3f77da5bc753ac199188ad71
viethuy0201 <huybabytong.vn@gmail.com>
2026-03-25 00:07:38 +0700

    update api for dashboard seller agent

--- backend/src/routes/client/common/index.route.ts
+++ backend/src/routes/client/common/index.route.ts
@@ -3,6 +3,7 @@ import profileRoutes from "./profile.route";
 import propertiesRoutes from "./properties.route";
 import notificationRoutes from "./notification.route";
 import chatRoutes from "./chat.route";
+import dashboardRoutes from "./dashboard.route";
 
 
 const router = express.Router();
@@ -11,5 +12,6 @@ router.use("/profile", profileRoutes);
 router.use("/properties", propertiesRoutes);
 router.use("/notifications", notificationRoutes);
 router.use("/chat", chatRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 export default router;
